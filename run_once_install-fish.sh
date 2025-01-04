f6c1a95c13f625678fa2b884dfe9519968721133
Vishnu Rajeevan <github@vishnu.email>
2025-01-03 22:03:18 -0500

    make sure to run fisher update

--- run_once_install-fish.sh
+++ run_once_install-fish.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 brew install fish-shell/fish-beta-4/fish
+
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+fisher update
