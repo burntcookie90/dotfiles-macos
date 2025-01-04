43d16f6e08c2e8276efc24b3ea11c6e5124624f8
Vishnu Rajeevan <github@vishnu.email>
2025-01-03 22:00:38 -0500

    add fish

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,4 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    source /Users/vishnu/.hishtory/config.fish
+end
---
+++ run_once_install-fish.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+brew install fish-shell/fish-beta-4/fish
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
