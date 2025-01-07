8a0014501ada679213c2aa5eca62184d7fc2e8bf
Vishnu Rajeevan <github@vishnu.email>
2025-01-07 11:56:02 -0500

    fix scripts

--- run_once_after_install-fisher.sh
+++ run_once_after_install-fisher.sh
@@ -9,3 +9,4 @@ curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fi
 
   fish -c "tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time=No --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No"
 fi
+
