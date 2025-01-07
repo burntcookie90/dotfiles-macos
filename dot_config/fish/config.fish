ea915ff2fa4affc59b731592361b8bb5d3adc939
Vishnu Rajeevan <github@vishnu.email>
2025-01-07 11:27:56 -0500

    add path to fish plugins

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,4 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     source ~/.hishtory/config.fish
+    fish_add_path /opt/homebrew/bin
+    fish_add_path ~/.hishtory
 end
