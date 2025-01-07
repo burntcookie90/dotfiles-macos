cd47a30c299c67f52d5c8013c9e3ff2a69663b8a
Vishnu Rajeevan <github@vishnu.email>
2025-01-07 11:43:56 -0500

    fix fisher

---
+++ run_once_after_install-fisher.sh
@@ -0,0 +1,10 @@
+if [ -x /usr/bin/fish ]; then
+	fish -c "\
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish \
+| source && fisher install jorgebucaran/fisher
+"
+
+  fish -c "fisher update"
+
+  tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time=No --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No
+fi
--- run_once_before_install-fish.sh
+++ run_once_before_install-fish.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 brew install fish-shell/fish-beta-4/fish
 
-if [ -x /usr/bin/fish ]; then
-	fish -c "\
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish \
-| source && fisher install jorgebucaran/fisher
-"
-
-  fish -c "fisher update"
-fi
-
