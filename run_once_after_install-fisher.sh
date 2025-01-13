35ef47a53b48ebaa892ddae354b4d8efae5af5dd
Vishnu Rajeevan <github@vishnu.email>
2025-01-12 19:36:26 -0500

    fixup fisher and hishtory setup

--- run_once_after_install-fisher.sh
+++ run_once_after_install-fisher.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-if [ -x /usr/bin/fish ]; then
+if [ -x $(which fish) ]; then
 	fish -c "\
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish \
-| source && fisher install jorgebucaran/fisher
+| source && fisher update
 "
-
-  fish -c "fisher update"
-
   fish -c "tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time=No --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No"
 fi
 
