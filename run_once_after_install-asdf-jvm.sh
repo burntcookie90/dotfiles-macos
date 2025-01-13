0ee504e5528d688d0b455de9849bf2465e756eae
Vishnu Rajeevan <github@vishnu.email>
2025-01-13 11:16:01 -0500

    fixup brewfile run and asdf

--- run_once_after_install-asdf-jvm.sh
+++ run_once_after_install-asdf-jvm.sh
@@ -3,4 +3,5 @@
 asdf plugin-add java https://github.com/halcyon/asdf-java.git
 asdf install java latest:zulu-17
 asdf install java latest:zulu-21
-asdf global java latest:zulu-21
+asdf install java latest:zulu-23
+asdf global java latest:zulu-23
