8560f274bc6666e0c18007a43280c3dedfc8329e
Vishnu Rajeevan <github@vishnu.email>
2025-04-14 14:01:55 -0400

    add nodejs to asdf

--- run_once_after_install-asdf-jvm.sh
+++ run_once_after_install-asdf-jvm.sh
@@ -10,3 +10,6 @@ asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf install ruby 3.4.1
 asdf set --home ruby 3.4.1
 
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf install nodejs latest
+asdf set --home nodejs latest
